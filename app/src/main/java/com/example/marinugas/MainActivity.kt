80fcc796e0a34a9817c19904fafc1fe878bb0295
putrija <putrijamalau23@gmail.com>
2023-12-18 22:05:07 +0700

    buat button pilih tanggal dan pilih jam untuk halaman detail_tugas -Putrija

--- app/src/main/java/com/example/marinugas/MainActivity.kt
+++ app/src/main/java/com/example/marinugas/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.marinugas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn_detail = findViewById<Button>(R.id.btn_detail)
+        btn_detail.setOnClickListener {
+            startActivity(Intent(this@MainActivity, detail_tugas::class.java))
+        }
     }
 }
