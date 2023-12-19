61c4ce352cf55084646965ff9eb53ff7d31b158a
gideonmnrg <89634118+gideonmnrg@users.noreply.github.com>
2023-12-19 12:56:40 +0700

    Retrofit 2
    Retrofit client & API

---
+++ app/src/androidTest/java/com/example/marinugas/API.kt
@@ -0,0 +1,17 @@
+import retrofit2.Call
+import com.example.marinugas.ListTugas
+import com.example.marinugas.ListTugasDetail
+import retrofit2.http.GET
+import retrofit2.http.Query
+import java.util.ArrayList // Add this import for ArrayList
+
+interface API {
+    @GET("read.php")
+    fun getListTugas(): Call<ArrayList<ListTugas>>
+
+    @GET("detail.php")
+    fun getListTugasDetail(
+        @Query("tugasId") tugasId: Int
+
+    ):Call<ListTugasDetail>
+}
---
+++ app/src/androidTest/java/com/example/marinugas/RetrofitClient.kt
@@ -0,0 +1,17 @@
+package com.example.marinugas
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    private const val BASE_URL ="http://192.168.100.204/test-retrofit/"
+
+    val instance: API by lazy {
+        val  retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        retrofit.create(API::class.java)
+    }
+}
